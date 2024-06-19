c71349fd59616561175765a3a5dbb3d2b57e6ed6
toeylomr <dethsakda.toey@lomr.co.th>
2024-06-19 23:39:52 +0700

    add mongodb

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const mongoURI = process.env.MONGO_URI;
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(mongoURI);
 
 app.use(express.json());
 
-app.post('/callback', (req, res) => {
+app.get('/', async (req, res) => {
+    res.status(200).send("Hello callback");
+});
+
+app.post('/callback', async (req, res) => {
     const callbackData = req.body;
     console.log('Received callback data:', callbackData);
 
-    res.status(200).send({ message: 'Callback received successfully', callbackData });
+    try {
+        await client.connect();
+        const db = client.db('gbcp');
+        const collection = db.collection('callback');
+        const result = await collection.insertOne(callbackData);
+        console.log('Saved to database:', result);
+        res.status(200).send({ message: 'Callback received and saved successfully' });
+    } catch (error) {
+        console.error('Error saving to database:', error);
+        res.status(500).send({ message: 'Failed to save callback data' });
+    } finally {
+        await client.close();
+    }
 });
+
 
 app.get('/invoke-callback', async (req, res) => {
     try {
